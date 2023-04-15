12b48e6fe2bd005c69dde0d3da3e89b3867561ef
Jura Mazar <ura2003mazar@gmail.com>
2023-04-15 15:22:21 +0300

    fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+
+buttonStart.addEventListener('click', onStartClick);
+buttonStop.addEventListener('click', onStopClick);
+
+const color = getRandomHexColor();
+
+function onStartClick() {
+    const timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        
+        if (!buttonStart.hasAttribute("disabled")) {
+            clearInterval(timerId)
+        }
+    }, 1000);
+    buttonStart.setAttribute("disabled", "true")
+};
+
+function onStopClick() {
+    if (buttonStart.hasAttribute("disabled")) {
+        buttonStart.removeAttribute("disabled")
+}
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
